239292a18176b6d8510007c9443bd48824caba94
RWitanski <witanski.robert@gmail.com>
2018-03-06 00:16:38 +0100

    Added Strobify project.

---
+++ Strobify/Model/GameController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Strobify.Model
+{
+    class GameController : INotifyPropertyChanged
+    {
+        string _name;
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (_name != value)
+                {
+                    _name = value;
+                    RaisePropertyChanged("Name");
+                }
+            }
+        }
+
+        Guid _deviceGuid;
+        public Guid DeviceGuid
+        {
+            get { return _deviceGuid; }
+            set
+            {
+                if (_deviceGuid != value)
+                {
+                    _deviceGuid = value;
+                    RaisePropertyChanged("DeviceGuid");
+                }
+            }
+        } 
+
+        void RaisePropertyChanged(string prop)
+        {
+            if (PropertyChanged != null) { PropertyChanged(this, new PropertyChangedEventArgs(prop)); }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
---
+++ Strobify/ViewModel/GameControllerViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Strobify.Model;
+using Strobify.Helpers;
+using System.Collections.ObjectModel;
+
+namespace Strobify.ViewModel
+{
+    class GameControllerViewModel : MainViewModel
+    {
+        public ObservableCollection<GameController> GameControllers { get; set; }
+
+        public RelayCommand GetGameControllers { get; set; }
+
+        public string GameControllerContent
+        {
+            get
+            {
+                return "Get game controllers";
+            }
+        }
+    }
+}
