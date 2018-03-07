0b4947b2fda48d08ae7f4c3243e0244e3d4e09d1
Witanski, Robert <robert.witanski@tectocom.com>
2018-03-07 11:08:05 +0100

    Added sample command to add device. Changed DataContext and removed Window.Resources in View.

--- Strobify/ViewModel/GameControllerViewModel.cs
+++ Strobify/ViewModel/GameControllerViewModel.cs
@@ -1,14 +1,14 @@
-﻿using System;
-using Strobify.Model;
+﻿using GalaSoft.MvvmLight;
 using Strobify.Helpers;
+using Strobify.Model;
+using System;
 using System.Collections.ObjectModel;
-using GalaSoft.MvvmLight;
 
 namespace Strobify.ViewModel
 {
     public class GameControllerViewModel : ViewModelBase
     {
-        public ObservableCollection<GameController> GameControllers { get; set; }
+        public ObservableCollection<GameController> GameControllers { get; set; } = new ObservableCollection<GameController>();
 
         object _selectedDevice;
         public object SelectedDevice
@@ -32,16 +32,23 @@ namespace Strobify.ViewModel
                 return "Get devices";
             }
         }
+
+        public void InitCommands()
+        {
+            this.GetDevicesCommand = new RelayCommand((parameter) =>
+                { InitGameControllerList(); }
+            );
+        }
+
+        private void InitGameControllerList()
+        {
+            GameControllers.Clear();
+            GameControllers.Add(new GameController { Name = "Kierownica", DeviceGuid = new Guid("13757675-0365-49DD-972D-D5954E7E7FD3") });
+        }
+
         public GameControllerViewModel()
         {
-            
-            GameControllers = new ObservableCollection<GameController>
-            {
-                    new GameController { Name="Kierownica" , DeviceGuid= new Guid("13757675-0365-49DD-972D-D5954E7E7FD3") },
-                    new GameController { Name="Joystick" , DeviceGuid= new Guid("852836DE-2A86-4192-AE8A-013BB11EA07F") },
-                    new GameController { Name="Joystick" , DeviceGuid= new Guid("DE33E027-8978-4E46-870C-A59FE105359C") },
-                    new GameController { Name="Fanatec" , DeviceGuid= new Guid("AE7A1C01-4545-4EDA-9F2B-3122B474F918") }
-            };
+            InitCommands();
         }
     }
 }
