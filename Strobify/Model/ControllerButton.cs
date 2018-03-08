d20e83f144259b8e963cec0c235a35eb0314f89b
Witanski, Robert <robert.witanski@tectocom.com>
2018-03-08 13:29:35 +0100

    Added Control button logic, update CommonServiceLocator.

---
+++ Strobify/Model/ControllerButton.cs
@@ -0,0 +1,9 @@
+﻿namespace Strobify.Model
+{
+    using WindowsInput.Native;
+    public class ControllerButton
+    {
+        public int DeviceButtonId { get; set; }
+        public VirtualKeyCode KeyboardKeyCode { get; set; }
+    }
+}
