cb74dcf48c6fda25bc53ecc3148b74e3ba6d0f0a
RWitanski <witanski.robert@gmail.com>
2018-03-16 01:27:37 +0100

    Added icons to refresh button and changed app view.

--- Strobify/App.xaml.cs
+++ Strobify/App.xaml.cs
@@ -1,10 +1,26 @@
 ﻿namespace Strobify
 {
+    using MahApps.Metro;
+    using System;
     using System.Windows;
+
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application
     {
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            // get the current app style (theme and accent) from the application
+            // you can then use the current theme and custom accent instead set a new theme
+            Tuple<AppTheme, Accent> appStyle = ThemeManager.DetectAppStyle(Application.Current);
+
+            // now set the Green accent and dark theme
+            ThemeManager.ChangeAppStyle(Application.Current,
+                                        ThemeManager.GetAccent("Orange"),
+                                        ThemeManager.GetAppTheme("BaseDark")); // or appStyle.Item1
+            base.OnStartup(e);
+        }
     }
 }
--- Strobify/ViewModel/GameControllerViewModel.cs
+++ Strobify/ViewModel/GameControllerViewModel.cs
@@ -16,6 +16,7 @@
         {
             this._deviceService = deviceService;
             InitCommands();
+            InitGameControllerList();
         }
 
         private GameController _selectedDevice;
@@ -31,19 +32,19 @@
             }
         }
 
-        private string _assignedControllerButtonText = "7";
-        public string AssignedControllerButtonText
+        private string _controllerButtonText = "7";
+        public string ControllerButtonText
         {
-            get { return _assignedControllerButtonText; }
-            set { Set(ref _assignedControllerButtonText, value); }
+            get { return _controllerButtonText; }
+            set { Set(ref _controllerButtonText, value); }
         }
-        private string _assignedKeyboardButtonText = "L";
-        public string AssignedKeyboardButtonText
+        private string _keyboardButtonText = "L";
+        public string KeyboardButtonText
         {
-            get { return _assignedKeyboardButtonText; }
+            get { return _keyboardButtonText; }
             set
             {
-                Set(ref _assignedKeyboardButtonText, value);
+                Set(ref _keyboardButtonText, value);
             }
         }
         private short _delay = 150;
@@ -62,29 +63,22 @@
 
         public RelayCommand GetDevicesCommand { get; private set; }
         public RelayCommand GetButtonIdCommand { get; private set; }
-        //public RelayCommand GetKeyboardButtonCommand { get; private set; }
         public RelayCommand StartCommand { get; private set; }
 
         public void InitCommands()
         {
-            this.GetDevicesCommand = new RelayCommand((parameter) =>
-                { InitGameControllerList(); }
+            this.GetDevicesCommand = new RelayCommand((parameter) => InitGameControllerList()
             );
-            this.GetButtonIdCommand = new RelayCommand((parameter) =>
-                { InitControllerButtonAssign(); }, (paramater) =>  true  // add switch to true if there is a selected Item to avoid exception.
+            this.GetButtonIdCommand = new RelayCommand((parameter) => InitControllerButtonAssign(), (paramater) => true  // add switch to true if there is a selected Item to avoid exception.
             );
-            //this.GetKeyboardButtonCommand = new RelayCommand((parameter) =>
-            //{ }, (parameter) => true
-            //);
-            this.StartCommand = new RelayCommand((parameter) =>
-                { StartLightService(); }, (parameter) => true
+            this.StartCommand = new RelayCommand((parameter) => StartLightService(), (parameter) => true
             );
         }
 
-        private void InitGameControllerList()
+        public void InitGameControllerList()
         {
             GameControllers.Clear();
-            
+
             foreach (var device in _deviceService.GetDevices())
             {
                 GameControllers.Add(device);
@@ -93,15 +87,15 @@
 
         private void InitControllerButtonAssign()
         {
-            _deviceService.AssignButtonsToController(SelectedDevice, AssignedKeyboardButtonText);
-            AssignedControllerButtonText = _deviceService.GameController.ControllerButton.DeviceButtonId.ToString();
+            _deviceService.AssignButtonsToController(SelectedDevice, KeyboardButtonText);
+            ControllerButtonText = _deviceService.GameController.ControllerButton.DeviceButtonId.ToString();
         }
 
         private void StartLightService()
         {
             _deviceService.Delay = Delay;
             _deviceService.Repeats = Repeats;
-            _deviceService.StartLightService(_selectedDevice, AssignedControllerButtonText, AssignedKeyboardButtonText);
+            _deviceService.StartLightService(_selectedDevice, ControllerButtonText, KeyboardButtonText);
         }
     }
 }
