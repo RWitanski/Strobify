315ea45b39d8e9b9034ec1d8f891d5ecab345b2b
RWitanski <witanski.robert@gmail.com>
2018-04-29 23:54:18 +0200

    Revert "base strategy cleanup."
    This reverts commit 02788f8531c21198f4c060942b678ce3736cf769.

--- Strobify/Strategies/ButtonMapperStrategy.cs
+++ Strobify/Strategies/ButtonMapperStrategy.cs
@@ -6,29 +6,14 @@
     public class ButtonMapperStrategy : IButtonMapperStrategy
     {
         private readonly IMessenger _messenger;
-
-        private readonly IControllerButtonMapper _controllerButtonMapper;
-        private readonly IKeyboardButtonMapper _keyboardButtonMapper;
-        public ButtonMapperStrategy(IMessenger messenger, IControllerButtonMapper controllerButtonMapper, IKeyboardButtonMapper keyboardButtonMapper)
+        public ButtonMapperStrategy(IMessenger messenger)
         {
-            this._messenger = messenger;
-            this._controllerButtonMapper = controllerButtonMapper;
-            this._keyboardButtonMapper = keyboardButtonMapper;
-            //ControllerButtonMapper = new ControllerButtonMapper(messenger);
-            //KeyboardButtonMapper = new KeyboardButtonMapper();
+            _messenger = messenger;
+            ControllerButtonMapper = new ControllerButtonMapper(messenger);
+            KeyboardButtonMapper = new KeyboardButtonMapper();
         }
-
-        //public ControllerButtonMapper ControllerButtonMapper { get; }
-        //public KeyboardButtonMapper KeyboardButtonMapper { get; }
-
-        public void MapKeyboardButton()
-        {
-
-        }
-
-        public void MapControllerButton()
-        {
-
-        }
+        
+        public ControllerButtonMapper ControllerButtonMapper { get; }
+        public KeyboardButtonMapper KeyboardButtonMapper { get; }
     }
 }
--- Strobify/Strategies/Interfaces/IButtonMapperStrategy.cs
+++ Strobify/Strategies/Interfaces/IButtonMapperStrategy.cs
@@ -2,5 +2,7 @@
 {
     public interface IButtonMapperStrategy
     {
+        ControllerButtonMapper ControllerButtonMapper { get; }
+        KeyboardButtonMapper KeyboardButtonMapper { get; }
     }
 }
